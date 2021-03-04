c52a864f754bfe0b849e1aed95366d2989a432a8
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-03-04 17:54:56 +0300

    create validation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,113 @@
+/* eslint-disable no-param-reassign */
 import _ from 'lodash';
+import onChange from 'on-change';
+import * as yup from 'yup';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import './index.html';
 
-/* function component() {
-  const element = document.createElement('div');
+const schema = yup.object().shape({
+  url: yup.string().url().required(),
+});
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hwwsdsdwwwwello', 'webpack'], ' ');
-  element.classList.add('hello');
+const errorMessages = {
+  network: {
+    error: 'Ресурс не содержит валидный RSS',
+  },
+  url: {
+    error: 'Ссылка должна быть валидным URL',
+  },
+};
 
-  return element;
-}
+const validate = (fields) => {
+  try {
+    schema.validateSync(fields, { abortEarly: false });
+    return '';
+  } catch {
+    return errorMessages.url.error;
+  }
+};
 
-document.body.appendChild(component());
- */
+const updateValidationState = (watchedState) => {
+  const error = validate(watchedState.form.fields);
+  watchedState.form.valid = _.isEqual(error, '');
+  watchedState.form.error = error;
+  console.log(watchedState.form.error);
+};
+
+const renderError = (input, error) => {
+  const feedbackEl = document.querySelector('.feedback');
+  feedbackEl.classList.add('text-danger');
+  feedbackEl.textContent = error;
+  input.classList.add('is-invalid');
+};
+
+const state = {
+  form: {
+    processState: 'filling',
+    processError: null,
+    fields: {
+      url: '',
+    },
+    valid: true,
+    error: '',
+  },
+};
+
+const form = document.querySelector('.rss-form');
+const input = document.querySelector('[aria-label="url"]');
+const submitButton = document.querySelector('.btn');
+
+const processStateHandler = (processState) => {
+  switch (processState) {
+    case 'failed':
+      submitButton.disabled = false;
+      break;
+    case 'filling':
+      submitButton.disabled = false;
+      break;
+    case 'sending':
+      submitButton.disabled = true;
+      break;
+    case 'finished':
+      container.innerHTML = 'User Created!';
+      break;
+    default:
+      throw new Error(`Unknown state: ${processState}`);
+  }
+};
+
+const watchedState = onChange(state, (path, value) => {
+  switch (path) {
+    case 'form.processState':
+      processStateHandler(value);
+      break;
+    case 'form.error':
+    case 'form.processError':
+      renderError(input, value);
+      break;
+    default:
+      break;
+  }
+});
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  watchedState.form.fields.url = input.value;
+  updateValidationState(watchedState);
+
+  if (_.isEqual(watchedState.form.error, '')) {
+    console.log('сендинг');
+    watchedState.form.processState = 'sending';
+    try {
+      const data = await axios.get(input.value);
+      watchedState.form.processState = 'finished';
+    } catch (err) {
+      watchedState.form.processError = errorMessages.network.error;
+      watchedState.form.processState = 'failed';
+      throw err;
+    }
+  }
+});
