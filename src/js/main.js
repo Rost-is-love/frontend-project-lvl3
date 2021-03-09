d9cd6cb69f98eda8069628f5d84b5791a5c21fab
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-03-09 21:39:06 +0300

    edit bootstrap connection

--- src/js/main.js
+++ src/js/main.js
@@ -13,7 +13,8 @@ import {
 } from './render.js';
 import createModal from './modal.js';
 import 'bootstrap/js/dist/modal';
-import '../style.scss';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 i18next.init({
   lng: 'ru',
