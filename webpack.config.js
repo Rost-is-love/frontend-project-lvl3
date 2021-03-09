2e5594195a5f108b0538b986fdb868bcb493bb5d
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-03-09 17:48:19 +0300

    back

--- webpack.config.js
+++ webpack.config.js
@@ -43,26 +43,3 @@ module.exports = {
     }),
   ],
 };
-
-/* const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-  entry: './src/index.js',
-  mode: process.env.NODE_ENV || 'development',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'template.html',
-    }),
-  ],
-}; */
