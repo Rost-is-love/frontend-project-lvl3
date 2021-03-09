7092b56002acbc6500328e48c80c2b4ea968c127
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-03-09 21:22:47 +0300

    add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ module.exports = {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
         test: /\.js$/,
         exclude: /node_modules/,
