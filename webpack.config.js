e4dee373210418be4df4ddc153d45f05a3e98894
Rostislav Antonov <antonov.qwertylox004@yandex.ru>
2021-03-12 12:23:29 +0300

    set up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,22 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
-const PATHS = {
-  src: path.join(__dirname, 'src'),
-  dist: path.join(__dirname, 'dist'),
-};
-
 module.exports = {
-  target: 'web',
   mode: process.env.NODE_ENV || 'development',
-  entry: {
-    main: PATHS.src,
-  },
-  output: {
-    path: PATHS.dist,
-    filename: '[name].js',
-  },
-  devtool: process.env.NODE_ENV === 'production' ? false : 'source-map',
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   devServer: {
-    historyApiFallback: true,
     contentBase: path.resolve(__dirname, 'dist'),
     open: true,
-    compress: true,
-    hot: true,
     port: 3000,
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
